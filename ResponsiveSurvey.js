0f0a046442452a640434436a7340267045c9f483
Sufyan Menk <80785081+smenk-dsg@users.noreply.github.com>
2022-05-25 15:37:52 -0500

    Update ResponsiveSurvey.js

--- ResponsiveSurvey.js
+++ ResponsiveSurvey.js
@@ -1,8 +1,3 @@
-import {Templates} from 'formiojs';
-
-Templates.framework = 'bootstrap'; // or whatever framework you want to use.
-
-// Then you can override specific templates with:
 Templates.current = {
   survey: {
     form: `<h1>test</h1>`
